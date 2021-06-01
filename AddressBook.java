6eabb026752301112fe5a9fdd29cd8796121d850
Anand27git <anandkumar256k@gmail.com>
2021-06-01 21:10:24 +0530

    adding contact details to AddressBook

--- AddressBook.java
+++ AddressBook.java
@@ -1,8 +1,11 @@
 public class AddressBook {
 
-public static void main(String args[]) {
-
-System.out.println("Welcome to AddressBook store All you contact Details");
+	public static void main(String args[]) {
+		//storing the  contact details
+		System.out.println("Welcome to AddressBook store All you contact Details");
+		AddressBookContacts contact = new AddressBookContacts("Anand", "Kumar", "Bangalore", "Bangarpet", "Karnataka",
+				"563114", "8088533154", "anandKumar256k@gmail.com");
+		contact.ContactDetails();
 
 	}
 }
