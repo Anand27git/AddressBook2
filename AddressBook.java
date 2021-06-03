1bdcefcd60cc525bf1e291d6f91aec8b34cca849
Anand27git <anandkumar256k@gmail.com>
2021-06-03 21:34:27 +0530

    add newContact

--- AddressBook.java
+++ AddressBook.java
@@ -1,11 +1,82 @@
+package com.bridgelabz.addressbook;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBook {
 
+	
+	private ArrayList<AddressBookContacts> addressContactList;
+
+	public AddressBook()
+	{
+		addressContactList = new ArrayList<AddressBookContacts>();
+		
+	}
+	
+	public void addContact(AddressBookContacts createPerson)
+	{
+		System.out.println("Adding a New Contact "+createPerson.firstname);
+		addressContactList.add(createPerson);
+		
+	}
+	
+	public AddressBookContacts getContactInput()
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your First Name");
+		String fname =sc.nextLine();
+		
+		System.out.println("Enter your Last Name");
+		String lname =sc.nextLine();
+		
+		System.out.println("Enter your Address");
+		String address =sc.nextLine();
+		
+		System.out.println("Enter your City");
+		String city =sc.nextLine();
+		
+		System.out.println("Enter your State");
+		String state = sc.nextLine();
+		
+		System.out.println("Enter your zipcode");
+		long zip =sc.nextLong();
+		System.out.println("Enter your Phone Number");
+		long phoneno =sc.nextLong();
+		
+		System.out.println("Enter your Email ID");
+		String email =sc.nextLine();
+		
+		AddressBookContacts contact = new  AddressBookContacts(fname,lname,address,city,state,zip,phoneno,email);
+		return contact;
+	}
+	
 	public static void main(String args[]) {
 		//storing the  contact details
 		System.out.println("Welcome to AddressBook store All you contact Details");
-		AddressBookContacts contact = new AddressBookContacts("Anand", "Kumar", "Bangalore", "Bangarpet", "Karnataka",
-				"563114", "8088533154", "anandKumar256k@gmail.com");
-		contact.ContactDetails();
-
+		int choice=0;
+		Scanner sc = new Scanner(System.in);
+		AddressBook ab = new AddressBook();
+		while(true)
+		{
+			System.out.println("1.Add Contact 2.Exit ");
+			System.out.println("Enter your Choice");
+			choice=sc.nextInt();
+		switch(choice)
+		{
+		case 1: 	AddressBookContacts addressbookcontacts = ab.getContactInput();
+					ab.addContact(addressbookcontacts);
+					System.out.println("Conact added Successfully");
+					break;
+					
+		case 2: 	System.out.println("you  are choosen to  exit ");
+					return;
+		
+		default:
+			
+				System.out.println("you have entered invaild choice");
+		}
+		
 	}
+}
 }
--- AddressBookContacts.java
+++ AddressBookContacts.java
@@ -1,30 +1,88 @@
-public class AddressBookContacts {
-	//declaring static variables
-	public final String firstname, lastname;
-	public final String address, city, state;
-	public final String zip;
-	public final String phonenumber;
-	public final String email;
+package com.bridgelabz.addressbook;
 
-	public AddressBookContacts(String firstname, String lastname, String address, String city, String state, String zip,String phonenumber, String email) {
+public class AddressBookContacts {
+	public String firstname, lastname, address, city, state, email;
+	public long zip;;
+	public long phoneno;
+
+	public AddressBookContacts(String firstname, String lastname, String address, String city, String state, long zip,
+			long phoneno, String email){
 		this.firstname = firstname;
 		this.lastname = lastname;
 		this.address = address;
 		this.city = city;
 		this.state = state;
-		this.zip = zip;
-		this.phonenumber = phonenumber;
 		this.email = email;
+		this.zip = zip;
+		this.phoneno = phoneno;
 	}
 
-	public void ContactDetails() {
-		System.out.println("First Name :" + firstname);
-		System.out.println("Last Name :" + lastname);
-		System.out.println("Address:" + address);
-		System.out.println("City:" + city);
-		System.out.println("State" + state);
-		System.out.println("Zip" + zip);
-		System.out.println("PhoneNumber" + phonenumber);
-		System.out.println("Email:" + email);
+	public String getFirstName() {
+		return firstname;
 	}
+
+	public String getLastName() {
+		return lastname;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public long getZip() {
+		return zip;
+	}
+
+	public long getPhoneNo() {
+		return phoneno;
+	}
+
+	public void setFirstName(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public void setLastName(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public void setAddress(String address) {
+			this.firstname = address;
+	}
+	public void setCity(String city) {
+				this.city=city;
+	}
+	public void setState(String state) {
+					this.state =state;
+	}
+	public void setEmail(String email) {
+						this.email = email;
+	}
+
+	public void setZip(long zip) {
+	this.zip = zip;
+	}
+	public void setPhoneNo(long phoneno) {
+		this.phoneno = phoneno;
+
+	}
+
+	@Override
+	public String toString() {
+		return "AddressBookContacts [firstname=" + firstname + ", lastname=" + lastname + ", address=" + address
+				+ ", city=" + city + ", state=" + state + ", email=" + email + ", zip=" + zip + ", phoneno=" + phoneno
+				+ "]";
+	}
+
 }
